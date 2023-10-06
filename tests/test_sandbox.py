d685b81eb965f42cb3f4886b3ffd805212ee4c35
Arnaldo Ono <git@onoarnaldo.com>
2023-10-06 14:08:45 +0200

    modify setup file

--- tests/test_sandbox.py
+++ tests/test_sandbox.py
@@ -8,7 +8,4 @@ JSON = ROOT.joinpath('data', 'sample_with_breakline.json')
 def test_values_with_breakline():
     data = html_to_json.from_file(HTML)
 
-    import json
-    print(json.dumps(data, indent=2))
-
     assert data == loads(JSON)
